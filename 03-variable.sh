085927ce11d2927ac142d218992bb8eaf2c97bae
rakesh <rakeshdigavamurthi@gmail.com>
2026-09-23 12:17:54 +0530

    hello world

---
+++ 03-variable.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+person1="rock"
+person2="dolly"
+
+echo "${person1}:hi ${person2} my name is rakesh"
+echo "${person2}:hi my name is ${person2}"
+echo "${person1}:what is your favorite color?"
+echo "${person2}:my favourite colour is black"
