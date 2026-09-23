42eca15ce2060b6bc1112472efabeda3fd9c1d75
rakesh <rakeshdigavamurthi@gmail.com>
2026-09-23 14:19:53 +0530

    hello world

--- 04-variables.sh
+++ 04-variables.sh
@@ -1,5 +1,9 @@
 #!bin/bash
 
-echo "please enter your username:rakesh:"
-read USER_NAME
+echo "please enter your username::"
+read USER_NAME # here USER_NAME is a variable which will store the value entered by the user
 echo "username is $USER_NAME"
+
+echo "please enter your password::"
+read -s PASSWORD # here PASSWORD is a variable which will store the value entered by the user
+echo "password is $PASSWORD"
