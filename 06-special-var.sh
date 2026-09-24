bd65da485e3e53c882b3e5c3beaaa14c9f58ec53
rakesh <rakeshdigavamurthi@gmail.com>
2026-09-24 13:43:53 +0530

    hello world

--- 06-special-var.sh
+++ 06-special-var.sh
@@ -5,3 +5,11 @@ echo "number of variables passed to the script: $#"
 
 echo "script name: $0"
 echo "process id of the script: $$"
+echo "who is running the script: $USER"
+echo "which directory the script is running from: $PWD"
+echo "home directory of the user running the script: $HOME"
+echo "pid of the current script: $$"
+sleep 5 &
+echo "pid of the background command running just now: $!"
+wait $!
+echo "line number of the script: $LINENO"
