f38cac17041fd229762524c49907b2f1b838db19
rakesh <rakeshdigavamurthi@gmail.com>
2026-09-24 13:09:54 +0530

    hello world

--- 06-special-var.sh
+++ 06-special-var.sh
@@ -2,3 +2,6 @@
 ##### special variables #####
 echo "all variables passed to the script: $@"
 echo "number of variables passed to the script: $#"
+
+echo "script name: $0"
+echo "process id of the script: $$"
