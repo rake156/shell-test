79b650640adcd09a502d287a1c204015c151eca1
rakesh <rakeshdigavamurthi@gmail.com>
2026-09-25 15:06:21 +0530

    hello world

--- 07-install.sh
+++ 07-install.sh
@@ -18,3 +18,19 @@ else
     echo "Installing MySQL is ... FAILED"
     exit 1
 fi
+
+dnf list installed nginx
+
+if [ $? -eq 0 ]; then
+    echo "Nginx is already installed"
+else
+    echo "Installing Nginx"
+    dnf install nginx -y
+
+    if [ $? -eq 0 ]; then
+        echo "Installing Nginx is ... SUCCESS"
+    else
+        echo "Installing Nginx is ... FAILED"
+        exit 1
+    fi
+fi
